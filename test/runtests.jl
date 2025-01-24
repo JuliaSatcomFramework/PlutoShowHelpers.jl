68836a605bd60e8a1eb99f60a824a70dddaca785
Alberto Mengali <disberd@gmail.com>
2025-01-24 19:22:33 +0100

    add some random test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,16 @@ using TestItemRunner
     using Aqua
     Aqua.test_all(PlutoShowHelpers)
 end
+
+@testitem "DualDisplayAngle" begin
+    d = DualDisplayAngle(π/2)
+
+    s = repr(d)
+    @test s === "90° (1.571 rad)"
+
+    @test s === repr(MIME"text/plain"(), d)
+
+    d = DualDisplayAngle(π/2, digits = 2)
+    s = repr(d)
+    @test s === "90° (1.57 rad)"
+end
